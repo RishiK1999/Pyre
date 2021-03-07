9b2abfa528e5206ba3575dab6e8cefd5e42ec541
RishiK1999 <rishikuhesan@gmail.com>
2021-03-06 20:29:51 -0500

    matching algo from scratch

--- Pyre.js
+++ Pyre.js
@@ -1,29 +1,29 @@
-class Rater
-{
-	constructor(@engine, @kind)
-    {
-        @db = new Bourne "./db-#{@kind}.json"
-    }
-	add: (user, item, done) ->
-	@db.find user: user, item: item, (err, res) =>
-		if res.length > 0
-			return done()
+alert('Hello Word');
 
-		@db.insert user: user, item: item, (err) =>
-			async.series [
-				(done) =>
-					@engine.similars.update user, done
-				(done) =>
-					@engine.suggestions.update user, done
-			], done
-            remove: (user, item, done) ->
-            @db.delete user: user, item: item, (err) =>
-                async.series [
-                    (done) =>
-                        @engine.similars.update user, done
-                    (done) =>
-                        @engine.suggestions.update user, done
-                ], done
-	itemsByUser: (user, done) ->
-	usersByItem: (item, done) ->
+var mentorProfiles = require('./mentor_profiles.json');
+var menteeProfiles = require('./mentee_profiles.json');
+
+var match = Array(mentorProfiles.profiles.length).fill(0);
+
+var mentorId;
+var menteeId;
+var spec;
+
+for (menteeId = 0;menteeId < menteeProfiles.profiles.length; menteeId++)
+{
+    for (mentorId = 0;mentorId < mentorProfiles.profiles.length; mentorId++)
+    {
+        var addPoints = 100;
+        for (spec = 0; spec < mentorProfiles.profiles[mentorId].specialization.length; spec++)
+        {
+            if (menteeProfiles.profiles[menteeId].specialization[spec] == mentorProfiles.profiles[mentorId].specialization[spec])
+            {
+                match[mentorId] += addPoints;
+            }
+            addPoints-=20;
+        }
+    }
 }
+
+var mentor1 = match.indexOf(Math.max(match));
+alert(mentor1);
